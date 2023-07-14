47c4be87b2547b32673a7b21d79c80ea40621b38
curious coddes <curiouscoddes@gmail.com>
2023-07-14 21:09:18 +0000

    Print digits 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: returns 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
