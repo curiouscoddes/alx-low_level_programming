fcd95c449212071f1917a9bfd4f7badeef368fc3
curious coddes <curiouscoddes@gmail.com>
2023-11-16 08:56:52 +0000

    Betty coding style

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,17 @@
 #include "main.h"
+
 /**
  * factorial - calculate factorial of a number
- * @n: parameter to be passed 
+ * @n:parameter to be passed
  * Return: return -1 for -ve no and 1 for 0 else return fact
  *
  */
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
 	if (n == 0)
-	{
 		return (1);
-	}
 	else
-	{
 		return (n * factorial(n - 1));
-	}
 }
